242d49a05ad6accbed5e46fa71be4a02fcc59a07
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-05-09 11:54:09 -0400

    starting configs

---
+++ input.js
@@ -0,0 +1,24 @@
+export class InputHandler {
+    constructor(){
+        this.keys = [];
+        window.addEventListener('keydown', e => {
+            if((e.key === "ArrowDown" || 
+                e.key === "ArrowUp") && 
+                this.keys.indexOf(e.key) === -1){
+                this.keys.push(e.key);
+            }
+            console.log(e.key, this.keys);
+        });
+        
+        window.addEventListener('keyup', e => {
+            if (e.key === "ArrowDown" || e.key === "ArrowUp"){
+                this.keys.splice(this.keys.indexOf(e.key), 1);
+            }
+            console.log(e.key, this.keys);
+        });
+
+    }
+}
+
+//In short: if the key is the arrow down key and the key is not already in the array, add it to the array.
+//Once you lift the key, its value is removed from the array.
---
+++ player.js
@@ -0,0 +1,18 @@
+export class Player {
+    constructor(game){
+        this.game = game;
+        this.width = 100;
+        this.height = 91.3
+        this.x = 0;
+        this.y = this.game.height - this.height;
+        this.image = document.getElementById('player');
+    }
+    update(){
+        //moves player
+        // this.x++;
+    }
+    draw(context){
+        //draws player
+        context.drawImage(this.image, 0, 0, 100, 91.3, this.x, this.y, this.width, this.height);
+    }
+}
