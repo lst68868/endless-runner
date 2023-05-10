c2018879dae1d2eb2d12a6ccce6eeeb20d390512
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-05-10 15:15:29 -0400

    dust particle effects debugged

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ window.addEventListener('load', function() {
             this.input = new InputHandler(this);
             this.UI = new UI(this);
             this.enemies = [];
+            this.particles = [];
             this.enemyTimer = 0;
             this.enemyInterval = 1000;
             this.debug = true;
@@ -45,7 +46,11 @@ window.addEventListener('load', function() {
                 enemy.update(deltaTime);
                 if(enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
             });
-
+            //handle particles
+            this.particles.forEach((particle, index) => {
+                particle.update();
+                if (particle.markedForDeletion) this.particles.splice(index, 1);
+            });
         }
         draw(context){
             //draw images, score, etc.
@@ -53,6 +58,9 @@ window.addEventListener('load', function() {
             this.player.draw(context);
             this.enemies.forEach(enemy => {
                 enemy.draw(context)
+            });
+            this.particles.forEach(particle => {
+                particle.draw(context);
             });
             this.UI.draw(context);
         }
@@ -65,7 +73,6 @@ window.addEventListener('load', function() {
     
 
     const game = new Game(canvas.width, canvas.height);
-    console.log(game);
     let lastTime = 0;
 
     function animate (timeStamp) {
